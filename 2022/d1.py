7f7d7298c1d94b7e7bd5f96a6eee72e67fb1b13a
Nikolett Jávor <nikolettjavor@gmail.com>
2022-12-01 09:54:34 +0100

    2022 d1 python

---
+++ 2022/d1.py
@@ -0,0 +1,12 @@
+elfs = []
+calories = 0
+with open("input.txt", "r", encoding="utf8") as f:
+    for sor in f:
+        if sor == "\n":
+            elfs.append(calories)
+            calories = 0
+        else: calories += int(sor)
+
+elfs.sort(reverse=True)
+print(elfs[0])
+print(elfs[0]+elfs[1]+elfs[2])
