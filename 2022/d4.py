8ab2c20ea3972bfe84bba05ea012eabe3247b40a
Nikolett Jávor <nikolettjavor@gmail.com>
2022-12-04 14:29:33 +0100

    2022 d4 python

---
+++ 2022/d4.py
@@ -0,0 +1,12 @@
+p1_fullcontain, p2_contain, pair = 0, 0, [0, 0]
+with open("input.txt", "r", encoding="utf8") as f:
+    for sor in f:
+        pair = sor.strip().split(",")
+        pair[0], pair[1] = [int(n) for n in pair[0].split("-")], [int(n) for n in pair[1].split("-")]
+        if (pair[0][0] <= pair[1][0] <= pair[0][1] and pair[0][0] <= pair[1][1] <= pair[0][1]) or (pair[1][0] <= pair[0][0] <= pair[1][1] and pair[1][0] <= pair[0][1] <= pair[1][1]):
+            p1_fullcontain += 1
+        if (pair[0][0] <= pair[1][0] <= pair[0][1] or pair[0][0] <= pair[1][1] <= pair[0][1]) or (pair[1][0] <= pair[0][0] <= pair[1][1] or pair[1][0] <= pair[0][1] <= pair[1][1]):
+            p2_contain += 1
+        pair.clear()
+
+print(f"part 1: {p1_fullcontain}\npart 2: {p2_contain}")
